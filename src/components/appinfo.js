848e7ccacdaa812d3780a4c10d1cc9713a13e8f6
stmoreau <st.moreau@outlook.com.gr>
2019-07-07 14:04:42 +0100

    update info

--- src/components/appinfo.js
+++ src/components/appinfo.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React from "react";
 
 const appinfo = () => (
   <div className="app__info">
-  {window.innerWidth > 540 ? (<div>
-    <h1 className="app__title">
-      Stephane Moreau
-    </h1>
-    <ul>
-      <li>👨‍💻 Senior Web Developer (2011-present)</li>
-      <li>🤵🏼 Technology Consultant / Operations Manager (2017-present)</li>
-      <li>🎓 MSc Student in Development Management (2018-present)</li>
-    </ul>
-    <div className="app_contribution">What's my day-to day contribution?</div>
-    <ul>
-      <li>🎯 Excellent skills in project & product management using agile methodologies</li>
-      <li>🎯 Passion about Consulting and Mentoring</li>
-      <li>🎯 Expert knowledge of HTML5/CSS3</li>
-      <li>🎯 jQuery, JavaScript, AJAX, JSON senior experience</li>
-      <li>🎯 ReactJS, Redux, ES6, Webpack and Babel senior experience</li>
-      <li>🎯 Up to date with latest UX/UI practices using Sketchapp</li>
-      <li>🎯 Experience developing server-side processes and automation scripts using Java, NodeJs, Python</li>
-      <li>🎯 Wide experience using a range of CMS like Wordpress, Joomla, Adobe Experience Manager</li>
-    </ul>
-  </div>) : ''}
+    {window.innerWidth > 540 ? (
+      <div>
+        <h1 className="app__title">Stephane Moreau</h1>
+        <ul>
+          <li>👨‍💻 Senior Web Developer (2011-present)</li>
+          <li>🎓 MSc Student in Development Management (2018-present)</li>
+        </ul>
+        <div className="app_contribution">
+          What's my day-to day contribution?
+        </div>
+        <ul>
+          <li>
+            🎯 Excellent skills in project & product management using agile
+            methodologies
+          </li>
+          <li>🎯 Passion about Consulting and Mentoring</li>
+          <li>🎯 Expert knowledge of HTML5/CSS3</li>
+          <li>🎯 jQuery, JavaScript, AJAX, JSON senior experience</li>
+          <li>🎯 ReactJS, Redux, ES6, Webpack and Babel senior experience</li>
+          <li>🎯 Up to date with latest UX/UI practices using Sketchapp</li>
+          <li>
+            🎯 Experience developing server-side processes and automation
+            scripts using Java, NodeJs, Python
+          </li>
+          <li>
+            🎯 Wide experience using a range of CMS like Wordpress, Joomla,
+            Adobe Experience Manager
+          </li>
+        </ul>
+      </div>
+    ) : (
+      ""
+    )}
   </div>
-)
+);
 
 export default appinfo;
